5979f34d7f0be5fd46bf2ff8918c9981efa860bb
Mark Petrie <petrie.mark@gmail.com>
2017-05-10 20:45:07 -0700

    Add working Add New Location form.

--- js/app.js
+++ js/app.js
@@ -9,12 +9,13 @@ function Location(locName, minHourlyCust, maxHourlyCust, avgCookiesCust, locatio
   this.locationHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
   this.hourlyCookieCounts = [];
   this.totalCookieCount = 0;
+  this.locationID;
 }
 
-// Function to calculate and set hourly cookie counts
+// setHourlyCookieCount function to calculate and set hourly cookie counts
 
 Location.prototype.setHourlyCookieCount = function (minHourlyCust, maxHourlyCust, avgCookiesCust, locationHours) {
-console.log(this);
+  console.log(this);
   for (var hour = 0; hour < this.locationHours.length; hour++) {
     var randomHourlyCust = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust)) + this.minHourlyCust;
     var hourlyCookieCount = Math.round(randomHourlyCust * this.avgCookiesCust);
@@ -23,7 +24,7 @@ console.log(this);
   }
 }
 
-// Render function to populate location cookie count data
+// Render function to populate HTML table with location cookie count data
 
 Location.prototype.render = function () {
   var table = document.getElementById('counts');
@@ -41,7 +42,34 @@ Location.prototype.render = function () {
   table.appendChild(row);
 }
 
-// Create locations using constructor function
+// Create new location from form submission
+
+var newLocation = document.getElementById('addLocation');
+newLocation.addEventListener('submit', addNewLocation);
+
+function addNewLocation() {
+  event.preventDefault();
+  var form = event.target;
+  console.log('event target: ' + event);
+  var locName = form.locName.value;
+  var minHourlyCust = form.minHourlyCust.value;
+  var maxHourlyCust = form.maxHourlyCust.value;
+  var avgCookiesCust = form.avgCookiesCust.value;
+  var locID = form.locID.value;
+  var locationHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
+  var hourlyCookieCounts = [];
+  var totalCookieCount = 0;
+
+  // Create new location using 
+  var newL = new Location(locName, minHourlyCust, maxHourlyCust, avgCookiesCust);
+
+  // 
+  newL.setHourlyCookieCount(minHourlyCust, maxHourlyCust, avgCookiesCust, locationHours);
+  newL.render();
+
+}
+
+// Generate original 5 locations using constructor function
 
 var pdxAirport = new Location('PDX Airport', 23, 65, 6.3);
 var pioneerSquare = new Location('Pioneer Square', 3, 24, 1.2);
@@ -49,11 +77,7 @@ var powells = new Location('Powell\'s', 11, 38, 3.7);
 var stJohns = new Location('St. John\'s', 20, 38, 2.3);
 var waterfront = new Location('Waterfront', 2, 16, 4.6);
 
-// Set hourly cookie counts for each location
-
-console.log('About to call PDX Airport');
 pdxAirport.setHourlyCookieCount(this.minHourlyCust, this.maxHourlyCust, this.avgCookiesCust, this.locationHours);
-console.log('After PDX Airport call');
 pioneerSquare.setHourlyCookieCount(this.minHourlyCust, this.maxHourlyCust, this.avgCookiesCust, this.locationHours);
 powells.setHourlyCookieCount(this.minHourlyCust, this.maxHourlyCust, this.avgCookiesCust, this.locationHours);
 stJohns.setHourlyCookieCount(this.minHourlyCust, this.maxHourlyCust, this.avgCookiesCust, this.locationHours);
